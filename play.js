8861a1264e32da0ad3affe0324b69865db29d4e3
Jea Oh Lee <jeaohlee99@gmail.com>
2022-09-28 14:14:59 -0700

    managed to make it work - changed 'key' and replaced with 'data'

--- play.js
+++ play.js
@@ -1,26 +1,6 @@
-const connect = require('./client');
+const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('connecting...');
-connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("key",  handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = (key) => {
-  if(key === '\u0003') {
-    process.exit();
-  } else if(key === 'w') {
-    con.write("Move: up");
-  } else if(key === 'a') {
-    con.write("Move: left");
-  } else if(key === 's') {
-    con.write("Move: down");
-  } else if(key === 'd') {
-    con.write("Move: right");
-  }
-}
+setupInput(connect());
